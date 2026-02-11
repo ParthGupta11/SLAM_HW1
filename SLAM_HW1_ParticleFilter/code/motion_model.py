56cdcdbb02164b7cfea41b695ac6c08df4647c7f
parth <parthg@andrew.cmu.edu>
2026-02-10 19:04:50 -0500

    [motion model] Initial implementation of odometry motion model
    TODO:
    - Fine-tune alpha values

--- SLAM_HW1_ParticleFilter/code/motion_model.py
+++ SLAM_HW1_ParticleFilter/code/motion_model.py
@@ -1,8 +1,8 @@
-'''
+"""
     Adapted from course 16831 (Statistical Techniques).
     Initially written by Paloma Sodhi (psodhi@cs.cmu.edu), 2018
     Updated by Wei Dong (weidong@andrew.cmu.edu), 2021
-'''
+"""
 
 import sys
 import numpy as np
@@ -14,16 +14,16 @@ class MotionModel:
     References: Thrun, Sebastian, Wolfram Burgard, and Dieter Fox. Probabilistic robotics. MIT press, 2005.
     [Chapter 5]
     """
+
     def __init__(self):
         """
         TODO : Tune Motion Model parameters here
         The original numbers are for reference but HAVE TO be tuned.
         """
-        self._alpha1 = 0.01
-        self._alpha2 = 0.01
-        self._alpha3 = 0.01
-        self._alpha4 = 0.01
-
+        self._alpha1 = 0.075
+        self._alpha2 = 0.075
+        self._alpha3 = 0.2
+        self._alpha4 = 0.075
 
     def update(self, u_t0, u_t1, x_t0):
         """
@@ -35,4 +35,31 @@ class MotionModel:
         """
         TODO : Add your code here
         """
-        return np.random.rand(3)
+        del_rot1 = math.atan2(u_t1[1] - u_t0[1], u_t1[0] - u_t0[0]) - u_t0[2]
+        del_trans = math.sqrt((u_t1[0] - u_t0[0]) ** 2 + (u_t1[1] - u_t0[1]) ** 2)
+        del_rot2 = u_t1[2] - u_t0[2] - del_rot1
+
+        del_rot1_hat = del_rot1 - np.random.normal(
+            0, np.sqrt(self._alpha1 * del_rot1**2 + self._alpha2 * del_trans**2)
+        )
+        del_trans_hat = del_trans - np.random.normal(
+            0,
+            np.sqrt(
+                self._alpha3 * del_trans**2
+                + self._alpha4 * del_rot1**2
+                + self._alpha4 * del_rot2**2
+            ),
+        )
+        del_rot2_hat = del_rot2 - np.random.normal(
+            0, np.sqrt(self._alpha1 * del_rot2**2 + self._alpha2 * del_trans**2)
+        )
+
+        x_t1 = np.zeros_like(x_t0)
+        x_t1[0] = x_t0[0] + del_trans_hat * math.cos(x_t0[2] + del_rot1_hat)
+        x_t1[1] = x_t0[1] + del_trans_hat * math.sin(x_t0[2] + del_rot1_hat)
+        x_t1[2] = x_t0[2] + del_rot1_hat + del_rot2_hat
+
+        # Angle wrapping to keep angles in range [-pi, pi]
+        x_t1[2] = math.atan2(math.sin(x_t1[2]), math.cos(x_t1[2]))
+
+        return x_t1
