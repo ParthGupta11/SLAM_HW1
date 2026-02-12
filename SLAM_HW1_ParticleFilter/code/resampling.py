4324a519c4f9fd301a7169e5244f1fd872bd6796
parth <parthg@andrew.cmu.edu>
2026-02-11 19:25:07 -0500

    [resampler] Implement low-variance resampling
    Note:
    - Normalizes weights
    - TODO: Reduce frequency of re-sampling
    - TODO: Disable resampling when robot is stationary

--- SLAM_HW1_ParticleFilter/code/resampling.py
+++ SLAM_HW1_ParticleFilter/code/resampling.py
@@ -1,8 +1,8 @@
-'''
+"""
     Adapted from course 16831 (Statistical Techniques).
     Initially written by Paloma Sodhi (psodhi@cs.cmu.edu), 2018
     Updated by Wei Dong (weidong@andrew.cmu.edu), 2021
-'''
+"""
 
 import numpy as np
 
@@ -12,6 +12,7 @@ class Resampling:
     References: Thrun, Sebastian, Wolfram Burgard, and Dieter Fox. Probabilistic robotics. MIT press, 2005.
     [Chapter 4.3]
     """
+
     def __init__(self):
         """
         TODO : Initialize resampling process parameters here
@@ -25,7 +26,7 @@ class Resampling:
         """
         TODO : Add your code here
         """
-        X_bar_resampled =  np.zeros_like(X_bar)
+        X_bar_resampled = np.zeros_like(X_bar)
         return X_bar_resampled
 
     def low_variance_sampler(self, X_bar):
@@ -36,5 +37,24 @@ class Resampling:
         """
         TODO : Add your code here
         """
-        X_bar_resampled =  np.zeros_like(X_bar)
+        X_bar_resampled = np.zeros_like(X_bar)
+
+        # Normalize weights to sum to 1
+        X_bar[:, 3] = X_bar[:, 3] / np.sum(X_bar[:, 3])
+
+        M = X_bar.shape[0]
+        r = np.random.uniform(0, 1.0 / M)
+        c = X_bar[0, 3]
+        i = 0
+        for m in range(M):
+            U = r + m * (1.0 / M)
+            while U > c:
+                i += 1
+                # Handle float point approximation issues
+                if i == M:
+                    i = M - 1
+                    break
+                c += X_bar[i, 3]
+            X_bar_resampled[m] = X_bar[i]
+
         return X_bar_resampled
